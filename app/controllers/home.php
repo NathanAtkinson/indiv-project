949b4a78bc50b82153f044b0b4a6c1a0771978e1
Nathan Atkinson <nathanatkinson@github.com>
2014-12-11 15:20:19 -0700

    Presenting errors in more user friendly way

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -20,8 +20,8 @@ extract($controller->view->vars);
                 <input class="user_name" type="text" name="user_name" title="Username">
                 <input class="password" type="password" name="password" data-exp-name="password" title="Password">
                 <input class="email" type="email" name="email" data-exp-name="email" title="E-mail">
-                <button class="sign-up">sign up</button>
                 <button class="submit">submit</button>
+                <button class="sign-up">sign up</button>
             </form>
             <div class="errormsg"></div>
         </div>
