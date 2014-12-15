ae2ff033537ea4164755fb24784ff1d462b631ad
Nathan Atkinson <nathanatkinson@github.com>
2014-12-15 14:02:34 -0700

    Cleaned up and commented map.js

--- js/map.js
+++ js/map.js
@@ -5,60 +5,55 @@ var map;
 var infoWindow;
 var service;
 
+//creates map, centers it on Long/Lat of address 1601 Fountainhead Pkwy
 function initialize() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: new google.maps.LatLng(33.403480, -111.964403),
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-    /*styles: [
-      {
-        stylers: [
-          { visibility: 'simplified' }
-        ]
-      },
-      {
-        elementType: 'labels',
-        stylers: [
-          { visibility: 'off' }
-        ]
-      }
-    ]*/
-  });
+    });
 
-  infoWindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
+    infoWindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
 
-  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+    google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
 }
 
+
+//searches based on location, a radius, and the query of pizza
 function performSearch() {
-  var request = {
-  	location: new google.maps.LatLng(33.403480, -111.964403),
+    var request = {
+    	location: new google.maps.LatLng(33.403480, -111.964403),
     // bounds: map.getBounds(),
     radius: '6000',
     query: 'pizza'
-  };
-  service.textSearch(request, callback);
+    };
+
+    //provides data in results that can be accessed below for address, name, etc.
+    service.textSearch(request, callback);
 }
+
 
 function callback(results, status) {
-  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
     alert(status);
     return;
-  }
-  for (var i = 0, result; result = results[i]; i++) {
+    }
+    for (var i = 0, result; result = results[i]; i++) {
     createMarker(result);
-  }
+    }
 }
 
+//creates markers on the map, using custom icon.  Also adds listener, so when icon is
+//clicked, shows details (rating, website, address, phone number)
 function createMarker(place) {
-  var marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location,
     icon: '/app/images/pizzaria.png'
-  });
+    });
 
-  google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, 'click', function() {
     service.getDetails(place, function(result, status) {
       if (status != google.maps.places.PlacesServiceStatus.OK) {
         alert(status);
@@ -70,12 +65,9 @@ function createMarker(place) {
       	+ "<br> Phone: " + result.formatted_phone_number);
       infoWindow.open(map, marker);
     });
-  });
+    });
 
 }
 
-
-
+//intitializes the map after page is loaded.
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
