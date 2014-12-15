1b8fb24dbde19790e295e736873f4052f2c68f46
Nathan Atkinson <nathanatkinson@github.com>
2014-12-15 11:11:59 -0700

    JS for map and also add/remove toppings

---
+++ js/map.js
@@ -0,0 +1,84 @@
+
+var map;
+var infoWindow;
+var service;
+
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: new google.maps.LatLng(33.403480, -111.964403),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    /*styles: [
+      {
+        stylers: [
+          { visibility: 'simplified' }
+        ]
+      },
+      {
+        elementType: 'labels',
+        stylers: [
+          { visibility: 'off' }
+        ]
+      }
+    ]*/
+  });
+
+
+  var request = {
+  placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+	};
+
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+  service.getDetails(request, callback);
+
+  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'pizza'
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    alert(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    createMarker(result);
+  }
+}
+
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: '/app/images/pizzaria.png'
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
+    });
+  });
+}
+
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
